8e227a9424c6b2b936660a607cc7860b9a0ab122
crspradlin <github@crspradlin.org>
2024-02-01 21:06:06 -0500

    Added check for transaction amount in email body

--- src/server/utils/emailFunctions.ts
+++ src/server/utils/emailFunctions.ts
@@ -12,9 +12,17 @@ const GetLatestUnreadPurchases = (): Purchase[] => {
         mail = GmailApp.search(`label:${props['EMAIL_UNREAD_LABEL']}`, index, 50);
         
         for (const thread of mail) {
-            const description = thread.getMessages()[0].getSubject();
-            const amountMatch = description.match(/\$([0-9,.]+)/);
+            const message = thread.getMessages()[0];
+            const subject = message.getSubject();
+            const body = message.getBody();
+
+            let amountMatch = subject.match(/\$([0-9,.]+)/);
+            if (amountMatch == null) {
+                amountMatch = body.match(/\$([0-9,.]+)/);
+            }
             const amount =  amountMatch == null ? 0.0 : parseFloat(amountMatch[1]);
+
+            let description = subject;
 
             const newPurchase = {
                 threadId: thread.getId(),
