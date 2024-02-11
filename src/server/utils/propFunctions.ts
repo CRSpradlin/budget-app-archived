ca607732a2bb1b9600598bb6467a3898dec019d4
Christopher Spradlin <38332276+CRSpradlin@users.noreply.github.com>
2024-02-10 21:46:31 -0500

    Add Increment Tip Buttons (#7)
    * Added tip increment buttons
    
    * Fixed predictive category issue with tip transactions

--- src/server/utils/propFunctions.ts
+++ src/server/utils/propFunctions.ts
@@ -11,7 +11,7 @@ const SetNewPurchaseCategoryUsage = (newPurchase: Purchase) => {
     const scriptProps = PropertiesService.getScriptProperties();
 
     const categoryMapping = JSON.parse(scriptProps.getProperty(propKey) || '{}');
-    const mappingDescription = newPurchase.description.replace(/\$([0-9,.]+)/, '');
+    const mappingDescription = newPurchase.description.replace(/\$([0-9,.]+)/, '').replace(/ \(Add. Tip: \$([0-9,.]+)\)/, '');
     categoryMapping[mappingDescription] = newPurchase.category;
 
     scriptProps.setProperty(propKey, JSON.stringify(categoryMapping));
